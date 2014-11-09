0fbaf90a3d04c9a2cbc8ca6c9b438246fab02d05
Juraj Hilje <jhilje@gmail.com>
2014-11-09 14:29:00 +0100

    Initial Commit

---
+++ GitHubReposSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubReposSwift
+//
+//  Created by Juraj Hilje on 09/11/14.
+//  Copyright (c) 2014 Juraj Hilje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
